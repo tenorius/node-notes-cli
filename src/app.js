533411b228d17ff3febadfc8a6fe451ede424217
rodrigo.o.tenorio <rodrigo.o.tenorio@accenture.com>
2018-09-01 00:43:52 -0300

    end of course

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
-const fs = require('fs');
-const os = require('os');
-const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes');
 
 console.log('Starting application...');
-
-const argv = yargs.argv;
+const titleOptions = {
+  describe: 'Title of note',
+  demand: true,
+  alias: 't',
+};
+const bodyOptions = {
+  describe: 'Body of note',
+  demand: true,
+  alias: 'b',
+};
+const argv = yargs
+  .command('add', 'Add a new note', {
+    title: titleOptions,
+    body: bodyOptions,
+  })
+  .command('list', 'List all notes')
+  .command('remove', 'Remove a note', {
+    title: titleOptions,
+  })
+  .help()
+  .argv;
 const command = process.argv[2];
 
 
